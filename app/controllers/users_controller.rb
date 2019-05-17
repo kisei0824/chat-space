a3dfedf0adc2474e73bdecb5f4db5be27de338c3
岩橋樹生 <iwahashikisei@iwahashimikionoMacBook-Pro.local>
2019-05-17 19:01:56 +0900

    Summary(repuired)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,27 @@
 class UsersController < ApplicationController
 
-    def index
-      @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+  def index
+   @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
    respond_to do |format|
      format.html
      format.json
     end
-    end
+  end
 
-    def edit
-    end
+  def edit
+  end
   
-    def update
-      if current_user.update(user_params)
-        redirect_to root_path
-      else
-        render :edit
-      end
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
     end
+  end
   
-    private
+  private
   
-    def user_params
-      params.require(:user).permit(:name, :email)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
