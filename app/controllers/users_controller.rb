7450caa07753a38a4e2bd754d0b8bddafeb4f52f
岩橋樹生 <iwahashikisei@iwahashimikionoMacBook-Pro.local>
2019-05-18 00:08:38 +0900

    Summary (repuired)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
 
+  def edit
+  end
+
   def index
    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
    respond_to do |format|
      format.html
      format.json
     end
-  end
-
-  def edit
   end
   
   def update
